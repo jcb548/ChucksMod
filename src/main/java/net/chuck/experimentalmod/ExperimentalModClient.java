35cbf7f00cf832d3222c28b98991d65e937bec9b
pieceoftrash8903 <56954478+pieceoftrash8903@users.noreply.github.com>
2023-08-08 00:06:43 +1000

    Created base for mod

---
+++ src/main/java/net/chuck/experimentalmod/ExperimentalModClient.java
@@ -0,0 +1,10 @@
+package net.chuck.experimentalmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExperimentalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
