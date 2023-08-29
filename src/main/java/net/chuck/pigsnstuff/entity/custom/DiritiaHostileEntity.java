711014977b6b577767863d47a343ac5663744e25
jcb548 <jacques.barons@gmail.com>
2023-08-30 02:36:40 +1000

    Diritia Hostile Entity attack plays but delayed

--- src/main/java/net/chuck/pigsnstuff/entity/custom/DiritiaHostileEntity.java
+++ src/main/java/net/chuck/pigsnstuff/entity/custom/DiritiaHostileEntity.java
@@ -2,6 +2,7 @@ package net.chuck.pigsnstuff.entity.custom;
 
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -13,13 +14,12 @@ import net.minecraft.world.World;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class DiritiaHostileEntity extends HostileEntity implements GeoEntity{
-    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
     public DiritiaHostileEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -39,18 +39,23 @@ public class DiritiaHostileEntity extends HostileEntity implements GeoEntity{
         this.goalSelector.add(4, new LookAroundGoal(this));
 
         targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+        targetSelector.add(2, new ActiveTargetGoal<>(this, LivingEntity.class, true));
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
         controllerRegistrar.add(new AnimationController<>(this, "controller",
-                0, this::predicate),
+                -20, this::predicate),
                 new AnimationController<>(this, "attackController",
-                0, this::attackPredicate));
+                -20, this::attackPredicate));
     }
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
-        if(tAnimationState.isMoving()){
+        /*if(tAnimationState.isMoving() && this.handSwinging){
+            tAnimationState.getController().setAnimation(RawAnimation.begin()
+                    .then("animation.diritia_hostile.attack", Animation.LoopType.LOOP));
+            return PlayState.CONTINUE;
+        } else */if(tAnimationState.isMoving()){
             tAnimationState.getController().setAnimation(RawAnimation.begin()
                     .then("animation.diritia_hostile.walk", Animation.LoopType.LOOP));
             return PlayState.CONTINUE;
@@ -62,10 +67,11 @@ public class DiritiaHostileEntity extends HostileEntity implements GeoEntity{
     }
 
     private <T extends GeoAnimatable> PlayState attackPredicate(AnimationState<T> tAnimationState){
-        if(this.handSwinging) {
+        if(this.isAttacking()) {
+            tAnimationState.getController().stop();
+            tAnimationState.getController().forceAnimationReset();
             tAnimationState.getController().setAnimation(RawAnimation.begin()
-                    .then("animation.diritia_hostile.attack", Animation.LoopType.PLAY_ONCE));
-            this.handSwinging = false;
+                    .thenPlay("animation.diritia_hostile.attack"));
         }
         return PlayState.CONTINUE;
     }
