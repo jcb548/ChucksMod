03f73f7a036915369f4e469767636a590f137b87
jcb548 <jacques.barons@gmail.com>
2024-05-05 15:55:36 +1000

    sound when extracting XP

--- src/main/java/net/chuck/chucksmod/block/entity/ExperienceInteractingFluidStoring.java
+++ src/main/java/net/chuck/chucksmod/block/entity/ExperienceInteractingFluidStoring.java
@@ -4,6 +4,8 @@ import net.chuck.chucksmod.fluid.ModFluids;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 
 public interface ExperienceInteractingFluidStoring extends FluidStoring{
@@ -14,7 +16,15 @@ public interface ExperienceInteractingFluidStoring extends FluidStoring{
         try(Transaction tx = Transaction.openOuter()){
             getFluidStorage().extract(FluidVariant.of(ModFluids.STILL_LIQUID_XP), extractAmount, tx);
             tx.commit();
+            int oldLevel = player.experienceLevel;
             player.addExperience((int)extractAmount);
+            if (player.experienceLevel/5 > oldLevel/5){
+                getWorld().playSound(null, getPos(), SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.BLOCKS,
+                        1.0f, 1.0f);
+            } else {
+                getWorld().playSound(null, getPos(), SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.BLOCKS,
+                        1.0f, 0.5f + getWorld().getRandom().nextFloat()*0.5f);
+            }
         }
     }
 }
